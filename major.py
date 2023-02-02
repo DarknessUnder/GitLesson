936fbcff854b44522dffbc129e6d95f1913d43c2
JS <67136819+skyjune0824@users.noreply.github.com>
2023-02-01 19:58:13 -0500

    Feel free to write

--- major.py
+++ major.py
@@ -3,9 +3,10 @@ attendanceList = [["Harry Tan", "tant2", "DarknessUnder"]]
 ## add your info here
 attendanceList.append(["Gabriel Jacob", "jacobggit", "????"])
 attendanceList.append(["Tingyi Tan", "tant", "ttan"])
+attendanceList.append(["Junseob Kim", "kimj43", "JKim"])
 
 ## DO NOT CHANGE
-printedList = open("attendanceListReal.txt", "w") 
+printedList = open("attendanceLeal.txt", "w") 
 
 for i in range(len(attendanceList)):
     printedList.write(str(i) + ". " + attendanceList[i][0] + "  RCS ID: " + attendanceList[i][1] + "  GitHub ID: " + attendanceList[i][2] + ";\n")
